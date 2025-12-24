309765f884595457a14a0a4caab4c97ee8d7817d
Kevin <kevinpinna13@gmail.com>
2025-12-24 12:24:42 +0100

    Setup Iniziale Progetto

---
+++ Backend/src/main/java/dev/kevin/mymed/MymedApplication.java
@@ -0,0 +1,13 @@
+package dev.kevin.mymed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymedApplication.class, args);
+	}
+
+}
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p className='text-red-600'>Hello World</p>
+    </>
+  )
+}
+
+export default App
